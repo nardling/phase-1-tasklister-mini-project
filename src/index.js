ec0cdbfa5f81537dc153662a1f6015e2ff2d360a
David <david.v.bernardo@gmail.com>
2022-09-22 11:18:50 -0500

    Add r/l click functionality

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
+const colors = ['green', 'yellow', 'red'];
 document.addEventListener("DOMContentLoaded", () => {
     // your code here
     const taskForm = document.getElementById("create-task-form");
     taskForm === null || taskForm === void 0 ? void 0 : taskForm.addEventListener("submit", (e) => {
         const submittedForm = e.target;
         e.preventDefault();
-        console.log("Form Submitted");
-        console.log(submittedForm["new-task-description"].value);
         const taskList = document.getElementById("tasks");
-        console.log(taskList);
         const newTask = document.createElement("li");
+        newTask.style.color = 'green';
+        newTask.setAttribute("data-state", "0");
         newTask.textContent = submittedForm["new-task-description"].value;
-        newTask.addEventListener("click", (e) => {
+        newTask.addEventListener("mousedown", (e) => {
             const self = e.target;
-            self.remove();
+            if (e.button == 0) {
+                self.remove();
+            }
+            else if (e.button == 2) {
+                e.preventDefault();
+                const curState = parseInt(self.getAttribute("data-state"));
+                const newState = (curState + 1) % 3;
+                self.setAttribute("data-state", newState.toString());
+                newTask.style.color = colors[newState];
+                return false;
+            }
+        });
+        newTask.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+            return false;
         });
         taskList.append(newTask);
     });
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
+const colors: string[] = ['green', 'yellow', 'red']
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const taskForm = document.getElementById("create-task-form");
   taskForm?.addEventListener("submit", (e) => {
     const submittedForm = e.target;
     e.preventDefault();
-    console.log("Form Submitted");
-    console.log(submittedForm["new-task-description"].value);
-    const taskList = document.getElementById("tasks")
-    console.log(taskList)
-    const newTask = document.createElement("li")
+    const taskList: HTMLElement = document.getElementById("tasks")
+    const newTask: HTMLElement = document.createElement("li")
+    newTask.style.color = 'green'
+    newTask.setAttribute("data-state", "0");
     newTask.textContent = submittedForm["new-task-description"].value
-    newTask.addEventListener("click", (e)=>{
+    newTask.addEventListener("mousedown", (e)=>{
       const self:HTMLElement = e.target as HTMLElement;
-      self.remove();
+      if (e.button == 0)
+      {
+        self.remove();
+      } else if (e.button == 2)
+      {
+        e.preventDefault()
+        const curState: number = parseInt(self.getAttribute("data-state"))
+        const newState: number = (curState + 1) % 3
+        self.setAttribute("data-state", newState.toString());
+        newTask.style.color = colors[newState]
+        return false;
+      }
     })
+    newTask.addEventListener("contextmenu", (e) => { 
+      e.preventDefault()
+      return false })
     taskList.append(newTask);
   })
 });
